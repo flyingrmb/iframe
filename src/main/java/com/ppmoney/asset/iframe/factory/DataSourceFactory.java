411739048300525732856f2effd6df1b0f44fd42
paul <wechat_pw@163.com>
2018-05-20 14:10:20 +0800

    Version 006: 集成配置文件

---
+++ src/main/java/com/ppmoney/asset/iframe/factory/DataSourceFactory.java
@@ -0,0 +1,41 @@
+package com.ppmoney.asset.iframe.factory;
+
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.lang.Nullable;
+
+import javax.sql.DataSource;
+import javax.validation.constraints.NotNull;
+
+/**
+ * Created by paul on 2018/5/20.
+ */
+public class DataSourceFactory implements FactoryBean<DataSource> {
+    @NotNull private String driverClassName;
+
+    @NotNull private String url;
+
+    @NotNull private String username;
+
+    @NotNull private String password;
+
+
+    @Nullable
+    @Override
+    public DataSource getObject() throws Exception {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://localhost/stg");
+        dataSource.setUsername("ppmoney");
+        dataSource.setPassword("ppmoney");
+
+        return dataSource;
+    }
+
+    @Nullable
+    @Override
+    public Class<?> getObjectType() {
+        return DataSource.class;
+    }
+}
