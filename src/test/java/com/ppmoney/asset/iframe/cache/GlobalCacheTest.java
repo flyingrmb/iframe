8ed7735ea304101689cd1e844e1cbcdaa0b185b7
paul <wechat_pw@163.com>
2018-05-20 12:40:54 +0800

    Version 006: 集成数据源.

--- src/test/java/com/ppmoney/asset/iframe/cache/GlobalCacheTest.java
+++ src/test/java/com/ppmoney/asset/iframe/cache/GlobalCacheTest.java
@@ -3,6 +3,11 @@ package com.ppmoney.asset.iframe.cache;
 import com.ppmoney.asset.iframe.entity.Identity;
 import org.junit.Test;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.*;
@@ -23,5 +28,28 @@ public class GlobalCacheTest {
 
         Integer intv2 = cache.get(identity, Integer.class);
         assertThat(intv2, is(nullValue()));
+    }
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Connection connection = null;
+        Statement stmt = null;
+
+        Class.forName("org.postgresql.Driver");
+        connection = DriverManager.getConnection("jdbc:postgresql://localhost/stg", "ppmoney", "ppmoney");
+        connection.setAutoCommit(false);
+        System.out.println("Opened database successfully");
+
+        stmt = connection.createStatement();
+        String sql = "CREATE TABLE STUDENT " +
+                "(ID TEXT PRIMARY KEY     NOT NULL ," +
+                " NAME            TEXT    NOT NULL, " +
+                " SEX             TEXT    NOT NULL, " +
+                " AGE             TEXT    NOT NULL)";
+        stmt.executeUpdate(sql);
+        System.out.println("Table created successfully");
+
+        stmt.close();
+        connection.commit();
+        connection.close();
     }
 }
